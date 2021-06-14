c6e3370d40020001d1861f9fbf17f4fe986b3e88
nonkronk <nonkronkblues@gmail.com>
2021-06-14 10:25:21 +0700

    #21

---
+++ SQL/easy/Select All.sql
@@ -0,0 +1,18 @@
+/*
+Select All
+https://www.hackerrank.com/challenges/select-all-sql/problem
+
+Query all columns (attributes) for every row in the CITY table.
+
+The CITY table is described as follows
+
+CITY
+FIELD TYPE
+ID NUMBER
+NAME VARCHAR2(17)
+COUNTRYCODE VARCHAR2(3)
+DISTRICT VARCHAR2(20)
+POPULATION NUMBER
+*/
+
+SELECT * FROM CITY
