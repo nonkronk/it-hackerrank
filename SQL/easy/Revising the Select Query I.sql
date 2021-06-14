95dc744d7332e70a2ee4d847c7eb07e26d15b16d
nonkronk <nonkronkblues@gmail.com>
2021-06-14 10:19:25 +0700

    #19

---
+++ SQL/easy/Revising the Select Query I.sql
@@ -0,0 +1,18 @@
+/*
+Revising the Select Query I
+https://www.hackerrank.com/challenges/revising-the-select-query/problem
+
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+The CITY table is described as follows:
+CITY
+FIELD TYPE
+ID NUMBER
+NAME VARCHAR2(17)
+COUNTRYCODE VARCHAR2(3)
+DISTRICT VARCHAR2(20)
+POPULATION NUMBER
+*/
+
+SELECT * FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
