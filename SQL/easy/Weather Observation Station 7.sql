97d4b367664c545f8dc6a45c5844ee69fa9c5902
nonkronk <nonkronkblues@gmail.com>
2021-06-14 10:53:32 +0700

    #30

---
+++ SQL/easy/Weather Observation Station 7.sql
@@ -0,0 +1,23 @@
+/*
+Weather Observation Station 7
+https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+STATION
+Field Type
+ID NUMBER
+CITY VARCHAR2(21)
+STATE VARCHAR2(2)
+LAT_N NUMBER
+LONG_W NUMBER
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+SELECT DISTINCT(CITY) FROM STATION 
+WHERE RIGHT(CITY,1) IN ("A", "I", "U", "E", "O");
