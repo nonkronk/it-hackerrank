da40d23c87d5884bb52e5c37eaba5e4db86396bb
nonkronk <nonkronkblues@gmail.com>
2021-06-14 11:17:31 +0700

    #37

---
+++ SQL/easy/Employee Names.sql
@@ -0,0 +1,36 @@
+/*
+Employee Names
+https://www.hackerrank.com/challenges/name-of-employees/problem?
+
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+Input Format
+
+The Employee table containing employee data for a company is described as follows:
+
+Column Type
+employee_id Integer
+name String
+months Integer
+salary Integer
+
+where employee_id is an employee's ID number, name is their name, months is the total number of months they've been working for the company, and salary is their monthly salary.
+
+Sample Input
+
+Sample Output
+
+Angela
+Bonnie
+Frank
+Joe
+Kimberly
+Lisa
+Michael
+Patrick
+Rose
+Todd
+*/
+
+SELECT name FROM Employee
+ORDER BY name;
