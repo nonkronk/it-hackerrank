e256cdfc69c06aa70c6a600602d772815d20736d
nonkronk <nonkronkblues@gmail.com>
2021-06-14 10:38:30 +0700

    #26

--- SQL/easy/Weather Observation Station 4.sql
+++ SQL/easy/Weather Observation Station 4.sql
@@ -1,6 +1,6 @@
 /*
 Weather Observation Station 4
-https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+https://www.hackerrank.com/challenges/weather-observation-station-4/problem
 
 Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 The STATION table is described as follows:
