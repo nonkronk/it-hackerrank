6e92e50f958d5a72d0cb43fc6404594f655d8964
nonkronk <nonkronkblues@gmail.com>
2021-06-14 10:33:02 +0700

    #24

---
+++ SQL/easy/Weather Observation Station 1.sql
@@ -0,0 +1,17 @@
+/*
+Weather Observation Station 1
+https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows:
+
+STATION
+Field Type
+ID NUMBER
+CITY VARCHAR2(21)
+STATE VARCHAR2(2)
+LAT_N NUMBER
+LONG_W NUMBER
+*/
+
+SELECT CITY, STATE FROM STATION
